8daa9b96f5023213e8a9f41413274fa1e5c5b3fb
Godwin peter .O <me@godwin.dev>
2020-05-11 06:38:23 +0100

    Update BaseView.php

--- Views/BaseView.php
+++ Views/BaseView.php
@@ -10,6 +10,12 @@ class BaseView {
         <html>
             <head>
                 <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+                <meta name="HandheldFriendly" content="true">
+                <!-- Load icon library -->
+                <link rel="stylesheet" href="/libs/style/main.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 <title>TDLOADS</title>
             </head>
             <body>
@@ -33,8 +39,12 @@ EOF;
     {
         return <<<EOF
 
-        <h1>My First Heading</h1>
-        <p>My first paragraph.</p>
+        <h1>Test Auth</h1>
+        <!-- The form -->
+        <form class="example" action="">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
         
 EOF;
     }
