fcffe97ac2d1f0bb344a4de1f526abe164d7b6b3
Godwin peter .O <me@godwin.dev>
2020-05-15 19:18:31 +0100

    Style changes

--- Views/AuthView.php
+++ Views/AuthView.php
@@ -13,19 +13,18 @@ final class AuthView extends BaseView {
             <div class="dl_status_icon">
                 <span class="material-icons dl_status_icon_error">error</span>
             </div>
-            <span class="dl_header_text">Requires PassKey</span>
+            <span class="dl_header_text" style="font-size: 22px;">Requires a PassKey</span>
         </div>
 
-        <form action="#" class="dl_form">
-            <div class="dl_base_form dl_vertical_center dl_horizontal_center">
+        <form action="#" class="dl_form dl_vertical_center dl_horizontal_center">
+            <div class="dl_base_form">
                 <div class="mdl-textfield mdl-js-textfield">
                     <input class="mdl-textfield__input dl_input" type="text" id="auth" required>
                     <label class="mdl-textfield__label" for="auth">Pass Key..</label>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored dl_button">
+                        <i class="material-icons">navigate_next</i>
+                    </button>
                 </div>
-
-                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored dl_button">
-                    <i class="material-icons">navigate_next</i>
-                </button>
             </div>
         </form>
     </div>
